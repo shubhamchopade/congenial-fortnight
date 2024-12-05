4d4ef373137293e6e4e034c9e3d47e986c1a4660
Sampada <sampadadhole4@gmail.com>
2024-12-04 23:16:17 -0500

    chore: Update USB device options in HTML form based on selected device

--- html_template.py
+++ html_template.py
@@ -33,7 +33,14 @@ class HTMLTemplate:
         template = HTMLTemplate.env.get_template('form.html')
         sidebar_items = HTMLTemplate.get_sidebar_items()
         usb_devices = HTMLTemplate.get_usb_devices()
-        return template.render(config=config, sidebar_items=sidebar_items, usb_devices=usb_devices)
+
+        # list of mp4 files for each usb device
+        usb_devices = ["1", "2", "3", "4"] # sample - to be replaced with actual usb devices by the naming 1_name, 2_name, 3_name, 4_name
+        mp4_1 = ["file1.mp4", "file2.mp4"] # sample
+        mp4_2 = ["file3.mp4", "file4.mp4"] # sample
+        mp4_3 = ["file5.mp4", "file6.mp4"] # sample
+        mp4_4 = ["file7.mp4", "file8.mp4"] # sample 
+        return template.render(config=config, sidebar_items=sidebar_items, usb_devices=usb_devices, mp4_1=mp4_1, mp4_2=mp4_2, mp4_3=mp4_3, mp4_4=mp4_4)
     
     @staticmethod
     def get_login_form():
